c18d92e059e17284e37b7a16daa3eb48e3650061
BBouleDeSuiff <emaouardi@gmail.com>
2022-12-02 16:35:48 +0100

    Finished update pos player

--- ShootEmUpSFML/ShootEmUpSFML/Object.h
+++ ShootEmUpSFML/ShootEmUpSFML/Object.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+//#include "SphereCollider.h"
 using namespace sf;
 
 
--- ShootEmUpSFML/ShootEmUpSFML/Player.cpp
+++ ShootEmUpSFML/ShootEmUpSFML/Player.cpp
@@ -7,32 +7,36 @@ Player:: Player(Vector2f _position, Vector2f _scale, float _angle, float _speed,
 }
 
 void Player:: Update(float time) {
-    Controls(player);
+    Controls(time, player);
 }
 
-void Player:: Controls(PlayerNumber playerNumber) {
+void Player:: Controls(float time, PlayerNumber playerNumber) {
     switch (playerNumber) {
         case PlayerNumber::PLAYER1:
-            UpdatePlayerPos(Keyboard::Q, Keyboard::D, Keyboard::S, Keyboard::Z);
+            UpdatePlayerPos(time, Keyboard::Q, Keyboard::D, Keyboard::S, Keyboard::Z);
             break;
         case PlayerNumber::PLAYER2:
-            UpdatePlayerPos(Keyboard::Left, Keyboard::Right, Keyboard::Down, Keyboard::Up);
+            UpdatePlayerPos(time, Keyboard::Left, Keyboard::Right, Keyboard::Down, Keyboard::Up);
             break;
     }
 }
 
-void Player:: UpdatePlayerPos(Keyboard::Key leftKey, Keyboard::Key rightKey, Keyboard::Key downKey, Keyboard::Key upKey) {
+void Player:: UpdatePlayerPos(float time, Keyboard::Key leftKey, Keyboard::Key rightKey, Keyboard::Key downKey, Keyboard::Key upKey) {
     this->position;
     if (sf::Keyboard::isKeyPressed(leftKey)) {
-        this->position.x -= 0.1f;
+        this->position.x += -1 * this->speed * time;
+        this->angle = -90;
     }
     else if (sf::Keyboard::isKeyPressed(rightKey)) {
-        this->position.x += 0.1f;
+        this->position.x += 1 * this->speed * time;
+        this->angle = 90;
     }
     else if (sf::Keyboard::isKeyPressed(downKey)) {
-        this->position.y -= 0.1f;
+        this->position.y -= 1 * this->speed * time;
+        this->angle = 0;
     }
     else if (sf::Keyboard::isKeyPressed(upKey)) {
-        this->position.y += 0.1f;
+        this->position.y += 1 * this->speed * time;
+        this->angle = 180;
     }
 }
