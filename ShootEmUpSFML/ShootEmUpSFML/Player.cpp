ddf621d29ad31e9aabb2fd54b5f99360f2dd1c8b
BBouleDeSuiff <emaouardi@gmail.com>
2022-12-02 15:35:19 +0100

    Player update position

--- ShootEmUpSFML/ShootEmUpSFML/Player.cpp
+++ ShootEmUpSFML/ShootEmUpSFML/Player.cpp
@@ -1,1 +1,39 @@
 #include "Player.h"
+
+void Player:: Update(float time) {
+    
+    Controls(player);
+
+}
+
+void Player:: Controls(PlayerNumber playerNumber) {
+    switch (playerNumber) {
+        case PlayerNumber::PLAYER1:
+            UpdatePlayerPos(Keyboard::Q, Keyboard::D, Keyboard::S, Keyboard::Z);
+            break;
+        case PlayerNumber::PLAYER2:
+            UpdatePlayerPos(Keyboard::Left, Keyboard::Right, Keyboard::Down, Keyboard::Up);
+            break;
+    }
+}
+
+void Player:: UpdatePlayerPos(Keyboard::Key leftKey, Keyboard::Key rightKey, Keyboard::Key downKey, Keyboard::Key upKey) {
+    this->position;
+    if (sf::Keyboard::isKeyPressed(leftKey)) {
+        this->position.x -= 0.1f;
+    }
+    else if (sf::Keyboard::isKeyPressed(rightKey)) {
+        this->position.x += 0.1f;
+    }
+    else if (sf::Keyboard::isKeyPressed(downKey)) {
+        this->position.y -= 0.1f;
+    }
+    else if (sf::Keyboard::isKeyPressed(upKey)) {
+        this->position.y += 0.1f;
+    }
+}
+
+
+main() {
+
+}
--- ShootEmUpSFML/ShootEmUpSFML/Player.h
+++ ShootEmUpSFML/ShootEmUpSFML/Player.h
@@ -1,6 +1,25 @@
 #pragma once
-class Player 
-{
+#include "Object.h"
 
+enum PlayerNumber {
+    PLAYER1,
+    PLAYER2,
+};
+
+class Player : public Object
+{
+	int life;
+
+    PlayerNumber player;
+
+    void Update(float time);
+    void Controls(PlayerNumber playerNumber);
+    void UpdatePlayerPos(Keyboard::Key leftKey, Keyboard::Key rightKey, Keyboard::Key downKey, Keyboard::Key upKey);
+
+    Player(Vector2f _position, Vector2f _scale, Vector2f _direction, float _angle, float _speed, int _life, PlayerNumber _player):Object(_position, _scale, _direction, _angle,  _speed )
+    {
+        player = _player;
+        life = 1;
+    }
 };
 
