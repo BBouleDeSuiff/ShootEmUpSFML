9facc71752c7f9386887a07d68ddd0afdf15873c
Victor9955 <v.lacombe.student@gmail.com>
2022-12-02 16:39:32 +0100

    Added SphereCollider and LineCollider and working collision

---
+++ ShootEmUpSFML/ShootEmUpSFML/SphereCollider.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "LineCollider.h"
+#include <cmath>
+
+class SphereCollider
+{
+public:
+	sf::Vector2f* position;
+	float radius;
+	SphereCollider(sf::Vector2f& pos, float rad);
+	bool CheckCollision(SphereCollider collider);
+	bool CheckCollision(sf::Vector2f point);
+	bool CheckCollision(LineCollider collider);
+};
