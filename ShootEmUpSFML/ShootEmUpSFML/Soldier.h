6df006358583849141c53d5e7311af236055bd66
BBouleDeSuiff <emaouardi@gmail.com>
2022-12-03 18:23:40 +0100

    Enemies classes added
    Soldier + Assassin

---
+++ ShootEmUpSFML/ShootEmUpSFML/Soldier.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Enemy.h"
+#include "Player.h"
+#include "Planet.h"
+
+class Soldier : public Enemy
+{
+	public:
+
+		void Update(float time);
+
+		Soldier(Vector2f _position, Vector2f _scale, float _angle, float _speed, Player& _p1, Player& _p2, Planet& _planet);
+};
