bb8e20d5e9074a5360e93520fbba2dccc04e6080
Chloe-Ms <mllechloe.mathias@gmail.com>
2022-12-05 16:41:40 +0100

    Stamina attack added

--- ShootEmUpSFML/ShootEmUpSFML/Line.cpp
+++ ShootEmUpSFML/ShootEmUpSFML/Line.cpp
@@ -1,23 +1,36 @@
 #include "Line.h"
 
 Line::Line(sf::Vector2f& playerOne, sf::Vector2f& playerTwo) {
-	stamina = 10.;
+	maxStamina = 10.;
+	stamina = maxStamina;
 	isActive = false;
 	this->playerOne = &playerOne;
 	this->playerTwo = &playerTwo;
 	line[0] = sf::Vertex(playerOne);
 	line[1] = sf::Vertex(playerTwo);	
 	collider = LineCollider(playerOne, playerTwo);
+	healthBar.setSize(sf::Vector2f(200,30));
+	healthBar.setPosition(sf::Vector2f(10,10));
+	healthBar.setFillColor(sf::Color::Green);
+	//healthBar.setPosition(sf::Vector2f());
 }
 void Line::Update(sf::Event& event, float time)
 {
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && stamina > 0.) {
 		isActive = true;
 		line[0] = sf::Vertex(*(this->playerOne));
 		line[1] = sf::Vertex(*(this->playerTwo));
+		stamina -= time * 2;
+		if (stamina <= 0.) {
+			isActive = false;
+		}
 	} else if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Space) {
 		isActive = false;
 	}
+	if (!isActive && stamina <= maxStamina) {
+		stamina += time * 3;
+	}
+	healthBar.setSize(sf::Vector2f(200 * stamina /maxStamina, 30));
 }
 
 void Line::Draw(sf::RenderWindow& window)
@@ -25,5 +38,6 @@ void Line::Draw(sf::RenderWindow& window)
 	if (isActive) {
 		window.draw(line, 2, sf::Lines);
 	}
+	window.draw(healthBar);
 }
 
--- ShootEmUpSFML/ShootEmUpSFML/Line.h
+++ ShootEmUpSFML/ShootEmUpSFML/Line.h
@@ -10,10 +10,11 @@ public :
 	void Draw(sf::RenderWindow& window);
 private:
 	float stamina;
+	float maxStamina;
 	bool isActive;
 	LineCollider collider;
 	sf::Vector2f* playerOne;
 	sf::Vector2f* playerTwo;
 	sf::Vertex line[2];
-
+	sf::RectangleShape healthBar;
 };
