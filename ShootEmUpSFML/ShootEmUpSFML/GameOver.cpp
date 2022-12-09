ca5701fe04ad81b641eba2cc52dd01c48d39d75c
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2022-12-09 15:02:36 +0100

    Begin Score And GamOver text

--- ShootEmUpSFML/ShootEmUpSFML/GameOver.cpp
+++ ShootEmUpSFML/ShootEmUpSFML/GameOver.cpp
@@ -6,12 +6,27 @@ GameOver::GameOver()
 {
 	playButton = Button(sf::Vector2f(400, 300), sf::Vector2f(200, 100), sf::Color::Red, sf::Color::White, 24, "Play");
 	quitButton = Button(sf::Vector2f(400,400), sf::Vector2f(200,100),sf::Color::Blue,sf::Color::White,24,"Quit");
+	font.loadFromFile("../Font.otf");
 	isOver = false;
 }
 
-void GameOver::GameOverUpdate(sf::RenderWindow& window)
+void GameOver::GameOverUpdate(sf::RenderWindow& window,int _score)
 {
+	score = _score;
 	window.clear(sf::Color::Black);
 	playButton.UpdateAndDraw(window);
 	quitButton.UpdateAndDraw(window);
+	DrawScore(sf::Vector2f(0,0),window);
+}
+
+void GameOver::DrawScore(sf::Vector2f _pos, sf::RenderWindow& window)
+{
+	scoreTxt.setPosition(_pos);
+	scoreTxt.setFont(font);
+	scoreTxt.setString("Score : " + std::to_string(score));
+	scoreTxt.setCharacterSize(24);
+	scoreTxt.setFillColor(sf::Color::White);
+
+	window.draw(scoreTxt);
+
 }
--- ShootEmUpSFML/ShootEmUpSFML/GameOver.h
+++ ShootEmUpSFML/ShootEmUpSFML/GameOver.h
@@ -6,8 +6,12 @@ class GameOver
 {
 public:
 	bool isOver;
+	int score;
 	Button playButton;
 	Button quitButton;
+	Text scoreTxt;
+	Font font;
 	GameOver();
-	void GameOverUpdate(sf::RenderWindow& window);
+	void GameOverUpdate(sf::RenderWindow& window, int _score);
+	void DrawScore(sf::Vector2f _pos, sf::RenderWindow& window);
 };
